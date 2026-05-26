63814c9823a5c4e6582564e30068e2709c6f72ab
Anhelina <krasunaangelinka@gmail.com>
2026-05-26 20:42:02 +0300

    update allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,11 @@ SECRET_KEY = 'django-insecure-kl0mn714yk)cs^kcbo*l5kx!wzl8kzfmn=^=3)y($jlg@a3_$5
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'web-3-lugd.onrender.com',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
